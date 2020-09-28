089a2e7b3d99da824553bc433b9da8170451ef9f
chaewss <60397314+chaewss@users.noreply.github.com>
2020-09-29 00:53:19 +0900

    Add files via upload

---
+++ W04-P/process_create_shop.php
@@ -0,0 +1,23 @@
+<?php
+  $link = mysqli_connect("localhost", "root", "rootroot", "dbp");
+  $filtered = array(
+    'name' => mysqli_real_escape_string($link, $_POST['name']),
+    'time' => mysqli_real_escape_string($link, $_POST['time']),
+    'phone' => mysqli_real_escape_string($link, $_POST['phone'])
+  );
+  $query = "
+  INSERT INTO shop
+  (name, time, phone)
+  VALUES (
+    '{$filtered['name']}',
+    '{$filtered['time']}',
+    '{$filtered['phone']}'
+  )";
+
+  $result = mysqli_query($link, $query);
+  if ($result == false) {
+      echo '저장하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+      error_log(mysqli_error($link));
+  } else {
+      header('Location: shop.php');
+  }
---
+++ W04-P/process_update_shop.php
@@ -0,0 +1,27 @@
+<?php
+  $link = mysqli_connect("localhost", "root", "rootroot", "dbp");
+  settype($_POST['id'], 'integer');
+  $filtered = array(
+    'id' => mysqli_real_escape_string($link, $_POST['id']),
+    'name' => mysqli_real_escape_string($link, $_POST['name']),
+    'time' => mysqli_real_escape_string($link, $_POST['time']),
+    'phone' => mysqli_real_escape_string($link, $_POST['phone'])
+  );
+
+  $query = "
+    UPDATE shop
+      SET
+        name = '{$filtered['name']}',
+        time = '{$filtered['time']}',
+        phone = '{$filtered['phone']}'
+      WHERE
+        id = '{$filtered['id']}'
+  ";
+
+  $result = mysqli_query($link, $query);
+  if ($result == false) {
+      echo '수정하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+      error_log(mysqli_error($link));
+  } else {
+      header('Location:shop.php?id='.$filtered['id']);
+  }
